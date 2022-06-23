0746d6de4104f0e32d31908234c1e75f61666aad
Jura Prokofev <Smyrke2005@yandex.ru>
2022-06-23 14:28:27 +0300

    Build fix

--- src/esp_functions.cpp
+++ src/esp_functions.cpp
@@ -527,7 +527,7 @@ namespace esp {
 #endif
 
 		pageTop = "<!DOCTYPE HTML><html><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><meta charset=\"utf-8\"/><script type=\"text/javascript\" src=\"/index.js\"></script><link rel=\"stylesheet\" type=\"text/css\" href=\"/index.css\"/>";
-		pageEndTop = "</head><body><hr size=\"1\">"
+		pageEndTop = "</head><body><hr size=\"1\">";
 		pageBottom = "<hr size=\"1\" class=\"red\"></body></html>";
 	}
 
